9fd53af3fca87cf0242d6d452a15d3ede889024b
tomtradeharlange-byte <tomtrade.harlange@gmail.com>
2026-04-17 22:45:13 +0200

    auto: sauvegarde 22:45:13

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { StatusBar, Style } from '@capacitor/status-bar'
+import { Capacitor } from '@capacitor/core'
+
+if (Capacitor.isNativePlatform()) {
+  StatusBar.setStyle({ style: Style.Dark })
+  StatusBar.setBackgroundColor({ color: '#faf2ef' })
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
